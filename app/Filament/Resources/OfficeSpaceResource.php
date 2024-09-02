38e48f57161630667aaa41e0dd6624b1272f52ba
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2024-09-02 23:26:15 +0800

    create crud office data

--- app/Filament/Resources/OfficeSpaceResource.php
+++ app/Filament/Resources/OfficeSpaceResource.php
@@ -6,9 +6,18 @@ use App\Filament\Resources\OfficeSpaceResource\Pages;
 use App\Filament\Resources\OfficeSpaceResource\RelationManagers;
 use App\Models\OfficeSpace;
 use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -23,7 +32,68 @@ class OfficeSpaceResource extends Resource
     {
         return $form
             ->schema([
-                //
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+
+                TextInput::make('address')
+                    ->required()
+                    ->maxLength(255),
+
+                FileUpload::make('thumbnail')
+                    ->image()
+                    ->required(),
+
+                Textarea::make('about')
+                    ->required()
+                    ->rows(10)
+                    ->cols(20),
+
+                Repeater::make('photos')
+                    ->relationship('photos')
+                    ->schema([
+                        FileUpload::make('photo')
+                            ->required()
+                    ]),
+
+                Repeater::make('benefits')
+                    ->relationship('benefits')
+                    ->schema([
+                        TextInput::make('name')
+                            ->required()
+                    ]),
+
+                Select::make('city_id')
+                    ->relationship('city', 'name')
+                    ->searchable()
+                    ->preload()
+                    ->required(),
+
+                TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->prefix('IDR'),
+
+                TextInput::make('duration')
+                    ->required()
+                    ->numeric()
+                    ->prefix('Days'),
+
+                Select::make('is_open')
+                    ->options([
+                        true => 'Open',
+                        false => 'Not Open'
+                    ])
+                    ->required(),
+
+                Select::make('is_full_booked')
+                    ->options([
+                        true => 'Not Available',
+                        false => 'Available'
+                    ])
+                    ->required()
+
+
             ]);
     }
 
@@ -32,9 +102,24 @@ class OfficeSpaceResource extends Resource
         return $table
             ->columns([
                 //
+                TextColumn::make('name')
+                    ->searchable(),
+
+                ImageColumn::make('thumbnail'),
+                TextColumn::make('city.name'),
+                IconColumn::make('is_full_booked')
+                    ->boolean()
+                    ->trueColor('danger')
+                    ->falseColor('success')
+                    ->trueIcon('heroicon-o-x-circle')
+                    ->falseIcon('heroicon-o-check-circle')
+                    ->label('Available'),
             ])
             ->filters([
                 //
+                SelectFilter::make('city_id')
+                    ->label('City')
+                    ->relationship('city', 'name')
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),

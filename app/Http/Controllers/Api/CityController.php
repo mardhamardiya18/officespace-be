a44020b074aeae4cfdbd43e1fb6405104338a559
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2024-09-07 22:50:13 +0800

    create City & Office Controller

--- app/Http/Controllers/Api/CityController.php
+++ app/Http/Controllers/Api/CityController.php
@@ -3,9 +3,25 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CityResource;
+use App\Models\City;
 use Illuminate\Http\Request;
 
 class CityController extends Controller
 {
     //
+    public function index()
+    {
+        $cities = City::withCount('officeSpaces')->get();
+
+        return CityResource::collection($cities);
+    }
+
+    public function show(City $city)
+    {
+        $city->load(['officeSpaces.city', 'officeSpaces.photos']);
+        $city->loadCount('officeSpaces');
+
+        return new CityResource($city);
+    }
 }
--- app/Http/Controllers/Api/OfficeSpaceController.php
+++ app/Http/Controllers/Api/OfficeSpaceController.php
@@ -3,9 +3,25 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\OfficeSpaceResource;
+use App\Models\OfficeSpace;
 use Illuminate\Http\Request;
 
 class OfficeSpaceController extends Controller
 {
     //
+    public function index()
+    {
+        $offices = OfficeSpace::withCount('city')->get();
+
+        return OfficeSpaceResource::collection($offices);
+    }
+
+    public function show(OfficeSpace $officeSpace)
+    {
+        $officeSpace->load(['city', 'photos', 'benefits']);
+        $officeSpace->loadCount('officeSpaces');
+
+        return new OfficeSpaceResource($officeSpace);
+    }
 }

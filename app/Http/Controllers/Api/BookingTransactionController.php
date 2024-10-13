9e4aec919605fe29d2fb832a729ea38c83d3fd59
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2024-10-14 01:29:38 +0800

    Fix bug booktransaction

--- app/Http/Controllers/Api/BookingTransactionController.php
+++ app/Http/Controllers/Api/BookingTransactionController.php
@@ -3,9 +3,31 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreBookingRequest;
+use App\Http\Resources\BookingTransactionResource;
+use App\Models\BookingTransaction;
+use App\Models\OfficeSpace;
 use Illuminate\Http\Request;
 
 class BookingTransactionController extends Controller
 {
     //
+
+    public function store(StoreBookingRequest $request)
+    {
+        $validatedData = $request->validated();
+
+        $officeSpace = OfficeSpace::find($validatedData['office_space_id']);
+
+        $validatedData['is_paid'] = false;
+        $validatedData['booking_trx_id'] = BookingTransaction::generateUniqueTrxId();
+        $validatedData['duration'] = $officeSpace->duration;
+
+        $validatedData['ended_at'] = (new \DateTime("{$validatedData['started_at']}"))->modify("+{$officeSpace->duration} days")->format('Y-m-d');
+
+        $bookingTransaction = BookingTransaction::create($validatedData);
+
+        $bookingTransaction->load('officeSpace');
+        return new BookingTransactionResource($bookingTransaction);
+    }
 }
--- app/Models/BookingTransaction.php
+++ app/Models/BookingTransaction.php
@@ -28,7 +28,7 @@ class BookingTransaction extends Model
 
         do {
             $randomString = $prefix . mt_rand(1000, 9999);
-        } while (self::where('booking_trx_id' . $randomString)->exists());
+        } while (self::where('booking_trx_id', $randomString)->exists());
 
         return $randomString;
     }
